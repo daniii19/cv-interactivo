e190d52072db0316ecac2a9607e1c35c8a0c98e0
daniii19 <danielgayolro@gmail.com>
2025-11-09 18:22:17 +0100

    Actualización

--- src/data/education.js
+++ src/data/education.js
@@ -1,5 +1,23 @@
 export const education = [
   {
+    title: "Curso de Especialización en Inteligencia Artificial y Big Data",
+    title_en: "Specialization Course in Artificial Intelligence and Big Data",
+
+    institution:
+      "IES Doctor Fleming, Oviedo",
+    institution_en:
+      "IES Doctor Fleming, Oviedo",
+
+    years: "2025 - 2026",
+
+    description:
+      "Formación especializada en inteligencia artificial y big data, incluyendo aprendizaje automático y el uso de herramientas y entornos de Big Data para la toma de decisiones inteligentes.",
+    description_en:
+      "Specialized training in artificial intelligence and big data, including machine learning and the use of Big Data tools and environments for intelligent decision-making.",
+
+    color: "purple",
+  },
+  {
     title: "Ciclo Formativo de Grado Superior en Desarrollo de Aplicaciones Web",
     title_en: "Higher Vocational Training in Web Application Development",
 
@@ -32,5 +50,5 @@ export const education = [
       "High school with a focus on technology, providing a solid foundation in science and applied mathematics.",
       
     color: "green",
-  },
+  }
 ];
